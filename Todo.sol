32abfebc0e20efde8a271ad888ec113dbef67e9d
Remix Workspace <>
2022-08-01 23:47:29 +0500

    todo

--- Todo.sol
+++ Todo.sol
@@ -3,17 +3,40 @@
 pragma solidity <0.9.0;
 
 contract Todo{
-    string[] todos;
-
-    function addTodo(string memory todo) public {
-        todos.push(todo);
+    struct todoObj {
+        uint id;
+        string todoText;
     }
 
-    function resetTodo() public{
-        todos = new string[](0);
+    todoObj[] todos;
+
+
+    function addTodo(uint _id,string memory todo) public {
+            todos.push(todoObj(_id,todo));
     }
 
-    function showTodo() public view returns(string[] memory){
+    function dltTodo(uint _id_) public{
+        for(uint i=0;i<todos.length;i++){
+            if(todos[i].id == _id_){
+                delete todos[i];
+            }
+        }
+    }
+
+
+    function showTodos() public view returns(todoObj[] memory){
         return todos;
     }
+
+
+    function updateTodos(uint id_,string memory new_todoText) public {
+        for(uint i =0;i<todos.length;i++){
+            if(todos[i].id == id_){
+                todos[i].todoText = new_todoText;
+            }
+        }
+    }
+    function resetAll() public{
+        delete todos;
+    }  
 }
